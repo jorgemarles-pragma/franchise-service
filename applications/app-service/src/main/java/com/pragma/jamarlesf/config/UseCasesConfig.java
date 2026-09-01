591ab5807719923e953a3f18d9fae8c427739ac1
Jorge Marles <jorge.marles@pragma.com.co>
2026-09-01 11:02:05 -0500

    Initial commit
    Scaffolding of the project

---
+++ applications/app-service/src/main/java/com/pragma/jamarlesf/config/UseCasesConfig.java
@@ -0,0 +1,14 @@
+package com.pragma.jamarlesf.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(basePackages = "com.pragma.jamarlesf.usecase",
+        includeFilters = {
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
+        },
+        useDefaultFilters = false)
+public class UseCasesConfig {
+}
